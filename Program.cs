914e668a25a50b9254b342755094e8dff0fb3a28
Niapa <niapa7@yandex.ru>
2023-06-14 15:36:52 +0700

    dz 2a commit

--- Program.cs
+++ Program.cs
@@ -1,15 +1,26 @@
 ﻿/* З №2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-a = 5; b = 7 -> max = 7*/
+a = 5; b = 7 -> max = 7
 Console.WriteLine("Введите первое число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-if (number<number1)
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1<num2)
 {
-    Console.WriteLine("max= number1");
-}
+    int max = num2;
+    Console.Write("max = ");
+    Console.Write(max);
+   }
 else
 {
-    Console.WriteLine("max=number");
+    int max = num1;
+    Console.WriteLine("max = ");
+    Console.Write(max);
 }
+З №4
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+*/
+
+
+
 
