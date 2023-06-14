52bbc8e37598adc8d65fece3c8d4e2e2f8ed10cf
Niapa <niapa7@yandex.ru>
2023-06-14 16:03:22 +0700

    dz 2b commit

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ else
 З №4
 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 2, 3, 7 -> 7
-*/
+
 Console.WriteLine("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
@@ -43,5 +43,19 @@ if (num1>num2)
     {
         Console.Write(num3);
     }
+
+  З №6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+*/
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2==0)
+{
+   Console.WriteLine("Это четное число"); 
+}
+else 
+{
+   Console.WriteLine("Это нечетное число"); 
+}
 
 
